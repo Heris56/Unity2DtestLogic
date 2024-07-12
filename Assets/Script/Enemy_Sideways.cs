e7228638e8063add80bf3ee5d308022cbdb55d0a
Haikal Risnandar <haikalrisnandar82@gmail.com>
2024-07-12 21:40:45 +0700

    menambahkan sistem health, healing, traps, dll

---
+++ Assets/Script/Enemy_Sideways.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class Enemy_Sideways : MonoBehaviour
+{
+    [SerializeField] private float damage;
+    [SerializeField] private float speed;
+    [SerializeField] private float movementDistance;
+    private bool movinLeft;
+    private float leftEdge;
+    private float rightEdge;
+
+    private void Awake()
+    {
+        leftEdge = transform.position.x - movementDistance;
+        rightEdge = transform.position.x + movementDistance;
+    }
+
+    private void Update()
+    {
+        if (movinLeft)
+        {
+            if (transform.position.x > leftEdge)
+            {
+                transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
+            }
+            else
+                movinLeft = false;
+        }
+        else
+        {
+            if (transform.position.x < rightEdge)
+            {
+                transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+            }
+            else
+                movinLeft = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            collision.GetComponent<Health>().TakeDamage(damage);
+        }
+    }
+}
