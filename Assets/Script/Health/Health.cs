92325c113f519cf6a2c3635f192a7e05e3e8a260
Haikal Risnandar <haikalrisnandar82@gmail.com>
2024-07-12 22:23:16 +0700

    menambahkan logic kebal selama beberapa detik setelah terkena damage + flashing effect

--- Assets/Script/Health/Health.cs
+++ Assets/Script/Health/Health.cs
@@ -1,16 +1,24 @@
+using System.Collections;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [Header ("Health")]
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
     private Animator anim;
     private bool dead;
 
+    [Header("Iframes")]
+    [SerializeField] private float iframesDuration;
+    [SerializeField] private int numberofFlashes;
+    private SpriteRenderer spriterend;
+
     private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriterend = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(float damage)
@@ -20,7 +28,7 @@ public class Health : MonoBehaviour
         if (currentHealth > 0)
         {
             anim.SetTrigger("hurt");
-            // iframes
+            StartCoroutine(Invulnerability());
         }
         else
         {
@@ -36,5 +44,19 @@ public class Health : MonoBehaviour
     public void addHealth(float health)
     {
         currentHealth = Mathf.Clamp(currentHealth + health, 0, startingHealth);
+    }
+
+    private IEnumerator Invulnerability()
+    {
+        Physics2D.IgnoreLayerCollision(8, 9, true);
+        // duration
+        for (int i = 0; i < numberofFlashes; i++)
+        {
+            spriterend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iframesDuration / (numberofFlashes * 2));
+            spriterend.color = Color.white;
+            yield return new WaitForSeconds(iframesDuration / (numberofFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(8, 9, false);
     }
 }
